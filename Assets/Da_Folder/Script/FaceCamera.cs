268fede3c42d2a52a2f5adaad0a5a9e41f3485e5
KanokradaSaeheng <132686137+KanokradaSaeheng@users.noreply.github.com>
2025-07-12 18:04:16 +0700

    wqwq
    wqr

---
+++ Assets/Da_Folder/Script/FaceCamera.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (mainCamera != null)
+        {
+            // Face the camera while keeping the UI upright
+            transform.rotation = Quaternion.LookRotation(transform.position - mainCamera.transform.position);
+        }
+    }
+}
