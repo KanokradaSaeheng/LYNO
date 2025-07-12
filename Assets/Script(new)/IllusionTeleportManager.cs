1badc8022d753800a10183b08c3541319380e9f5
Poupav <thanachot10072550@gmail.com>
2025-07-12 18:19:05 +0700

    Fix+update
    Illusion complete!!!

---
+++ Assets/Script(new)/IllusionTeleportManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class IllusionTeleportManager : MonoBehaviour
+{
+    public static IllusionTeleportManager Instance;
+    private List<IllusionTeleportByViewIndex> teleportZones = new List<IllusionTeleportByViewIndex>();
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public void RegisterZone(IllusionTeleportByViewIndex zone)
+    {
+        if (!teleportZones.Contains(zone))
+            teleportZones.Add(zone);
+    }
+
+    public void UnregisterZone(IllusionTeleportByViewIndex zone)
+    {
+        teleportZones.Remove(zone);
+    }
+
+    public IllusionTeleportByViewIndex GetActiveTeleportZone()
+    {
+        foreach (var zone in teleportZones)
+        {
+            if (zone.IsPlayerInZone())
+                return zone;
+        }
+        return null;
+    }
+}
